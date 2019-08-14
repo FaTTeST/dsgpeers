617e6b9cf3c2db07520043ce70f2f54b061aad9a
FaTTeST <fattest123@yandex.ru>
2019-08-15 00:27:39 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+define('_TEMPLATE_DIR', 'templates');
+include_once('templates\dsg_def\common.php');
---
+++ templates/dsg_def/common.php
@@ -0,0 +1,5 @@
+<?php
+include_once('view\header.html');
+include_once('view\body.html');
+include_once('view\footer.html');
+
